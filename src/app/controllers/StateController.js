e8014ffbd96d7c24b5a950ea8b0ffb493c4d5be9
rodriguesporan <rodriguesporan@gmail.com>
2019-07-02 13:03:34 -0300

    Adicionando validação

--- src/app/controllers/StateController.js
+++ src/app/controllers/StateController.js
@@ -1,7 +1,15 @@
+const yup = require('yup');
 const State = require('../models/stateModel');
 
 class StateController {
   async store(req, res) {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      uf: yup.string().required(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     try {
       const state = await State.create(req.body);
       return res.json(state);
